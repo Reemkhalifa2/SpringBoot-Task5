6bc2602f853d05a94e9dc7a12132668998e4554c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-07 12:28:33 +0400

    first commit

---
+++ src/test/java/com/example/Task5Demo/Task5DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task5Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task5DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
